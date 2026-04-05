4bcd078e1bc39e449017565f9b5dc24b16926389
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2026-04-06 02:56:41 +0300

    Time: 0 ms (100%), Space: 19.3 MB (33.2%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -3,14 +3,4 @@ class Solution:
         r = 0
         c = 0
 
-        for m in moves:
-            if m == "U":
-                r -= 1
-            if m == "D":
-                r += 1
-            if m == "R":
-                c += 1
-            if m == "L":
-                c -= 1
-
-        return r == 0 and c == 0
+        return moves.count("U") == moves.count("D") and moves.count("R") == moves.count("L")
