288102866701860cac719b189b2d8c6b212e64b4
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2026-05-20 00:52:17 +0300

    Time: 7 ms (85.13%), Space: 42 MB (19.35%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,5 +1,8 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        if nums1[0] > nums2[-1] or nums1[-1] < nums2[0]:
+            return -1
+
         mp = defaultdict(int)
 
         for num in nums1:
